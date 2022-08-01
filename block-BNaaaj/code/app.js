325098415e9416da4373f9372f4f23760f41987c
SMITHAN KUMAR R <smithankumar404@gmail.com>
2022-08-01 21:02:48 +0530

    core node Assignment 1

---
+++ block-BNaaaj/code/app.js
@@ -0,0 +1,10 @@
+let areaOfSquares = require('areaOfRectangle');
+let areaOfCircle = require('areaOfSquare');
+let areaOfRectangle = require('areaOfCircle');
+
+console.log(areaOfSquares,areaOfCircle,areaOfRectangle); 
+
+let buff = Buffer.alloc(12);
+Buffer.write("created new Buffer");
+console.log(buff.toString());
+
