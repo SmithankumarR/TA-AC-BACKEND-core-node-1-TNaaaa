1b96aabf16b13a7b5c6fb26b2d4ff733c523a363
SMITHAN KUMAR R <smithankumar404@gmail.com>
2022-08-02 19:55:25 +0530

    readFile and streams

--- block-BNaaat/code/server.js
+++ block-BNaaat/code/server.js
@@ -1,19 +1,19 @@
 var http = require('http');
 var fs = require('fs');
 
-function handleRequest(req,res){
-    if(req.method === "GET" && req.ul === '/'){
+function handleRequest(req, res) {
+    if (req.method === "GET" && req.ul === '/') {
         res.setHeader('Content-Type', 'text/plain');
         res.end('Welcome to index page');
-        
-    } else if(req.method === "GET" && req.url === '/about'){
+
+    } else if (req.method === "GET" && req.url === '/stream') {
         res.setHeader('Content-Type', 'text/html');
-        fs.createReadStream('./about.html').pipe(res);
-    } else if(req.method === "POST" && req.url === '/about'){
-        res.setHeader('Content-Type', 'text/html');
-        fs.readFile('./about.html', 'utf8', (err,content) => {
-            if(err) console.log(err);
-            res(content);
+        fs.createReadStream('./node.html').pipe(res);
+    } else if (req.method === "GET" && req.url === '/file') {
+        fs.readFile('./node.html', 'utf8', (err, content) => {
+            if (err) console.log(err);
+            res.setHeader('Content-Type', 'text/html');
+            res.end(content);
         })
     } else {
         res.setHeader('Content-Type', 'text/plain');
