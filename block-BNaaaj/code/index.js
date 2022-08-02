371d6834e8882570ba9d90016a306bd03af09869
SMITHAN KUMAR R <smithankumar404@gmail.com>
2022-08-02 11:09:21 +0530

    Node Assignment 1

--- block-BNaaaj/code/index.js
+++ block-BNaaaj/code/index.js
@@ -1,25 +1,15 @@
-console.log('Welcome to Nodejs');
-const {readFile , readFileSync} = require('fs');
-
 let os = require('os');
 
-let cpus = console.log(os.cpus());
+console.log('Welcome to Nodejs');
+
+// const { readFile, unlink } = require('fs');
+
+
+let cpus = console.log(os.cpus().length);
 let freem = console.log(os.freemem());
 let upTime = console.log(os.uptime());
-let version = console.log(os.version());
 
-console.log(cpus,freem,upTime,version);
+console.log(cpus, freem, upTime);
 
-readFile('./unlink.md', 'utf8', (err,content) => {
-    if (err) throw err;
-    console.log(content);
-})
-let synCode = readFileSync('./unlink.md', 'utf8');
 
-setTimeout(() => console.log(synCode), 1000);
 
-let url = require('url');
-
-let parsedUrl = url.parse('https://airindia.com/fares/calculate?from=delhi&to=detroit');
-console.log(parsedUrl);
-console.log(parsedUrl.query,parsedUrl.pathname,parsedUrl.protocol);
